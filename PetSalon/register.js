a8ceef794b0c8af82af7a437ee172f186640dab0
Wendou90 <107042515+Wendou90@users.noreply.github.com>
2022-07-27 13:28:28 -0400

    Add files via upload

---
+++ PetSalon/register.js
@@ -0,0 +1,67 @@
+let petSalon ={
+    name:"The nice pets",
+    address:"514 lib ave",
+    pets:[
+        {
+            name:"bobby",
+            age:2,
+            breed: "love",
+            gender: "male",
+            service:"Grooming",
+            ownerName:"momo",
+            contactPhone:"77777887",
+        },
+        {
+            name:"job",
+            age:3,
+            breed:"shiwawa",
+            gender:"female",
+            service:"vaccination",
+            ownerName:"jaques",
+            contactPhone:"666778856",
+        },
+        {
+            name:"vandamme",
+            age:4,
+            breed:"german",
+            gender:"male",
+            service:"shot",
+            ownerName:"Bret",
+            contactPhone:"667778854",
+
+        },
+    ]
+
+}
+function Pets(name,age,breed,gender,service,ownerName,contactPhone){
+    this.name=name;
+    this.age=age;
+    this.breed=breed;
+    this.gender=gender;
+    this.service=service;
+    this.owner=ownerName;
+    this.contact=contactPhone;
+}
+let baabi = new Pets("baabi",10,"french","male","grooming","charles",776655777);
+for(let i=0;i<petSalon.pets.length;i++){
+    console.log(petSalon.pets[i].name)
+}
+function register(){
+    document.getElementById("txtName").value="";
+    document.getElementById("numAge").value="";
+    document.getElementById("txtBreed").value="";
+    document.getElementById("txtGender").value="";
+    document.getElementById("txtService").value="";
+    document.getElementById("txtOwner").value="";
+    document.getElementById("numPhone").value="";
+}
+let newpet = new Pets(name,age,breed,gender,service,ownerName,contactPhone)
+function displayInfo(){
+    document.getElementById("info").innerHTML=`Welcome to the ${petSalon.name}family.`;
+}
+     
+function displayNumberOfPets(){
+    document.getElementById("number").innerHTML=`There are ${petSalon.pets.length} pets`;
+}
+displayInfo();
+displayNumberOfPets();
